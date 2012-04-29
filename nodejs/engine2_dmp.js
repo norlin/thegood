ba135954ec836d213d52290f906fc2a15c75b6e4
norlin <myxaxaxa@gmail.com>
2012-04-29 18:42:30 +0400

    and now…

--- nodejs/engine2_dmp.js
+++ nodejs/engine2_dmp.js
@@ -675,7 +675,8 @@ sys.log(12);
 						}
 
 					},
-					oninfo:function (err,data){
+					oninfo:function (err,data){,
+						var oauth = {};
 sys.log(13);
 						if (err) {
 							Interface.status = 502;
